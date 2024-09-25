fff6c1ec7790121f6ed7efd44a1a1d3eba25b4ca
Luai Saif <eng.luaish@gmail.com>
2024-09-25 20:26:13 +1000

    Refactor: removing Duplicated styles

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,7 +5,6 @@ import GameCardSkeleton from "./GameCArdSkeleton";
 
 export default function GameGrid() {
   const { games, error, isLoading } = useGames();
-  console.log(isLoading)
   const skeletons = [1,2,3,4,5,6]
   return (
     <>
